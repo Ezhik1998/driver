aaf1455c634139b8ed8020a8a53adcf37b453a52
Ezhik1998 <tmovlian@gmail.com>
2020-04-02 00:29:03 +0300

    Get users info from Cloud Firestore

--- lib/pages/statisticPage.dart
+++ lib/pages/statisticPage.dart
@@ -5,13 +5,12 @@ import 'package:flutter/foundation.dart';
 import 'package:driver/services/firebaseAuthUtils.dart';
 
 class StatisticPage extends StatefulWidget {
-  StatisticPage(
-      {Key key, this.auth, this.onSignedOut, this.userId, this.userEmail})
+  StatisticPage({Key key, this.auth, this.onSignedOut, this.userId})
       : super(key: key);
 
   final AuthFunc auth;
   final VoidCallback onSignedOut;
-  final String userId, userEmail;
+  final String userId;
 
   @override
   _StatisticPageState createState() => _StatisticPageState();
@@ -27,15 +26,23 @@ class _StatisticPageState extends State<StatisticPage> {
     // _checkEmailVerification();
   }
 
-  Future<void> _getUserInfo(String id) async {
-    var snapshot =
-        await Firestore.instance.collection('users').document(id).get();
-    print(snapshot.data);
-  }
+  // Future<Map<String, dynamic>> _getUserInfo(String id) async {
+  //   var snapshot =
+  //       await Firestore.instance.collection('users').document(id).get();
+  //   print(snapshot.data);
+  //   return snapshot.data;
+  // }
+
+  // getUserData(AsyncSnapshot<DocumentSnapshot> snapshot) {
+  //   // return snapshot.data.documents
+  //   //     .map((doc) => ListTile(
+  //   //         title: Text(doc["name"]), subtitle: Text(doc["email"].toString())))
+  //   //     .toList();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    _getUserInfo(widget.userId);
+    // _getUserInfo(widget.userId);
     return Scaffold(
       // appBar: AppBar(
       //   title: Text("Flutter Auth Email"),
@@ -68,24 +75,44 @@ class _StatisticPageState extends State<StatisticPage> {
                 fit: BoxFit.fill,
               ),
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Center(
-                  child: Text(
-                    "Hello " + widget.userEmail,
-                    style: TextStyle(fontSize: 26.0, color: Colors.white),
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    "Your id: " + widget.userId,
-                    style: TextStyle(fontSize: 18.0, color: Colors.white),
-                  ),
-                )
-              ],
-            ),
+            child: StreamBuilder<DocumentSnapshot>(
+                stream: Firestore.instance
+                    .collection("users")
+                    .document(widget.userId)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting)
+                    return new Center(child: new CircularProgressIndicator());
+                  if (snapshot.hasData) {
+                    // return Text(snapshot.data.data["name"]);
+                    // // print(snapshot.data.data["name"]);
+                    // return Container();
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Center(
+                          child: Text(
+                            "Hello " + snapshot.data["name"],
+                            style:
+                                TextStyle(fontSize: 26.0, color: Colors.white),
+                          ),
+                        ),
+                        Center(
+                          child: Text(
+                            "Your email: " + snapshot.data["email"],
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.white),
+                          ),
+                        )
+                      ],
+                    );
+                  } else {
+                    return new Center(
+                      child: new Text('Error', style: TextStyle(color: Colors.red),),
+                    );
+                  }
+                }),
           ),
           Positioned(
             width: MediaQuery.of(context).size.width,
